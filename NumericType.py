06d194fff1abfce6889db5b3304606bf6d6a2a41
Sachin Kumar <67166839+devsachink22@users.noreply.github.com>
2025-03-03 19:38:55 +0530

    Update NumericType.py

--- NumericType.py
+++ NumericType.py
@@ -1,1 +1,29 @@
+i=8 #i is a variable into which data of int type is stored
+print(i) #print the value stored in variable i
+print(type(i)) #print the type/class of value stored in variable i
 
+f=6.2 #f is a variable into which data of float type is stored
+print(f) #print the value stored in variable f
+print(type(f)) #print the type/class of value stored in variable f
+
+c=8-2j #c is a variable into which data of complex type is stored
+print(c) #print the value stored in variable c
+print(type(c)) #print the type/class of value stored in variable c
+
+x=0b11 #x is a variable into which data of binary type is stored
+print(x) #print the value stored in variable x
+print(type(x)) #print the type/class of value stored in variable x
+
+y=0XBA #y is a variable into which data of hexadecimal type is stored
+print(y) #print the value stored in variable y
+print(type(y)) #print the type/class of value stored in variable y
+
+z=0o117 #z is a variable into which data of octadecimal type is stored
+print(z) #print the value stored in variable z
+print(type(z)) #print the type/class of value stored in variable z
+
+'''
+Binary type prefix with 0b or 0B
+Hexadecimal type prefix with 0x or 0X
+Octadecimal type prefix with 0o or 0O
+'''
