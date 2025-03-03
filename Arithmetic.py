037b5f6e1852fbd0d2d78c35aaf96a5d5b09bbbd
Sachin Kumar <67166839+devsachink22@users.noreply.github.com>
2025-03-03 19:23:05 +0530

    Create Arithmetic.py

---
+++ Arithmetic.py
@@ -0,0 +1,6 @@
+print(2+3) #used to perform add operation
+print(8-5) #used to perform subtract operation
+print(7*3.9) #used to perform multiply operation
+print(5/2.5) #used to perform divide operation
+print(2+5/3*6-4.7) #used to perform order of operations (PEMDAS - Parentheses Exponents Multiply Divide Add Subtract)
+print('2+4/8.3') #operations enclosed in inverted commas are considered as string
