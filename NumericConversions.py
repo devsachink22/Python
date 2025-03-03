bed28ce97d075fd332393c0f9be6c861134507b9
Sachin Kumar <67166839+devsachink22@users.noreply.github.com>
2025-03-03 20:03:11 +0530

    Update NumericConversions.py

--- NumericConversions.py
+++ NumericConversions.py
@@ -1,1 +1,24 @@
+i=10 #int type
+f=float(i) #conversion step of int type to float type
+print(f) #print the value stored in variable f
+print(type(f)) #print the type/class of data stored in variable f
 
+f=17.99 #float type
+i=int(f) #conversion step of float type to int type
+print(i) #print the value stored in variable i
+print(type(i)) #print the type/class of data stored in variable i
+
+s="46.21" #string type
+f=float(s) #conversion step of string type to float type
+print(f) #print the value stored in variable f
+print(type(f)) #print the type/class of data stored in variable f
+i=int(f) #conversion step of float type to int type
+print(i)  #print the value stored in variable i
+print(type(i)) #print the type/class of data stored in variable i
+
+a=156 #int type
+print(bin(a)) #conversion step of int type to binary type
+print(hex(a)) #conversion step of int type to hexadecimal type
+print(oct(a)) #conversion step of int type to octadecimal type
+
+"""String of decimal type can not be directly converted into integer. Firstly, it should be converted into float & then int.
