2f316db80fa65c278d50b93f65c1da3c98187950
Sachin Kumar <67166839+devsachink22@users.noreply.github.com>
2025-03-04 00:35:37 +0530

    Create FormattedString.py

---
+++ FormattedString.py
@@ -0,0 +1,4 @@
+name='Sachin Kumar' #string
+age=25 #int
+print(f'My name is',name,'\nMy age is',age) #formatted string used to print the variable
+print(f'My name is {name}\nMy age is {age}') #ormatted string used to print the variable using curly braces
